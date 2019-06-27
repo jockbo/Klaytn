8496c425ce9b36e92e6c9e06c119e3e74bfc31ae
jeewonlee <z1top133@naver.com>
2019-06-28 02:44:40 +0900

    klaytn page

---
+++ truffle.js
@@ -0,0 +1,16 @@
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = `https://api.baobab.klaytn.net:8651`
+const PRIVATE_KEY = '0xd4c90a7109690d2903b2b35ba6fd75963e4724846af6fd86aed7b0e52397fb4a'
+
+module.exports = {
+  networks: {  
+    klaytn: {
+      provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    }
+  },
+}
