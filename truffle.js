af8b017da1f00ea69926c2d2e0f219218df9600c
jeewonlee <z1top133@naver.com>
2019-06-28 06:31:13 +0900

    change private key

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ const PrivateKeyConnector = require('connect-privkey-to-provider')
 const NETWORK_ID = '1001'
 const GASLIMIT = '20000000'
 const URL = `https://api.baobab.klaytn.net:8651`
-const PRIVATE_KEY = '0xd4c90a7109690d2903b2b35ba6fd75963e4724846af6fd86aed7b0e52397fb4a'
+const PRIVATE_KEY = '0x872d14619aa2587d343f2af44718fb65928cb1ce6fb3b99c5d3974b2a0025ef8'
 
 module.exports = {
   networks: {  
